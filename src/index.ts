cbb2e1cf452d05a9e4ac195ff8a9c3c4e357e260
Okinea Dev <hi@okinea.dev>
2025-10-05 19:23:24 +0200

    feat: make output identical as in `changelogithub` and improve internal commit filtering

--- src/index.ts
+++ src/index.ts
@@ -68,34 +68,93 @@ export async function generateChangelog(
 	Handlebars.registerHelper('splitLines', (text: string) => text.split(/\r?\n/))
 	Handlebars.registerHelper('upperFirst', upperFirst)
 
+	// Allowed authors
+	const allAuthors = commits
+		.flatMap((commit) => commit.author)
+		.filter(
+			(author, index, array) =>
+				index === array.findIndex((a) => a.name.trim().toLowerCase() === author.name.trim().toLowerCase()),
+		)
+		.filter((author) => {
+			if (config.excludeBots && /\[bot\]$/i.test(author.name.trim())) return false
+			if (typeof config.excludeContributors === 'function') return !config.excludeContributors(author)
+			if (Array.isArray(config.excludeContributors)) {
+				if (config.excludeContributors.includes(author.name)) return false
+				if (config.excludeContributors.includes(author.email)) return false
+			}
+			return true
+		})
+
+	const filteredCommits = commits.filter((commit) =>
+		allAuthors.some((author) => author.name === commit.author.name && author.email === commit.author.email),
+	)
+
 	const sections: Section[] = (
 		await Promise.all(
 			Object.entries(config.types)
 				.filter((type): type is [string, string] => type[1] !== false)
 				.map(async ([type, title]) => {
 					const commits_ = await Promise.all(
-						commits
+						filteredCommits
 							.filter((e) => e.type === type)
+							.map((commit) => ({
+								...commit,
+								coAuthors: commit.coAuthors.filter(
+									(coAuthor) =>
+										coAuthor.email !== commit.author.email && coAuthor.name !== commit.author.name,
+									// !Object.keys(commit.author).every(
+									// 	(key) =>
+									// 		Object.hasOwn(commit.author, key) &&
+									// 		coAuthor[key as keyof GitCommitAuthor] ===
+									// 			commit.author[key as keyof GitCommitAuthor],
+									// ),
+								),
+							}))
 							.map(async (commit) => {
-								if (config.resolveContributorsGitHub) {
-									const resolvedAuthorGithub = await githubUser(commit.author.email)
-									if (resolvedAuthorGithub) {
-										return {
-											...commit,
-											author: {
-												...commit.author,
-												name: resolvedAuthorGithub.name || commit.author.name,
-												username: resolvedAuthorGithub.username,
-											} as ResolvedGitCommitAuthor,
-										}
-									}
+								let author = commit.author
+								let coAuthors = commit.coAuthors
+
+								const [resolvedAuthorGithub, resolvedCoAuthors] = await Promise.all([
+									config.resolveContributorsGitHub ? githubUser(commit.author.email) : Promise.resolve(null),
+									config.contributors && commit.coAuthors?.length
+										? Promise.all(
+												commit.coAuthors.map(async (co) => {
+													const resolved = await githubUser(co.email)
+													return resolved
+														? ({
+																...co,
+																name: resolved.name || co.name,
+																username: resolved.username,
+															} as ResolvedGitCommitAuthor)
+														: co
+												}),
+											)
+										: Promise.resolve(commit.coAuthors),
+								])
+
+								if (resolvedAuthorGithub) {
+									author = {
+										...commit.author,
+										name: resolvedAuthorGithub.name || commit.author.name,
+										username: resolvedAuthorGithub.username,
+									} as ResolvedGitCommitAuthor
 								}
-								return commit
+
+								coAuthors = resolvedCoAuthors
+
+								return {
+									...commit,
+									author,
+									coAuthors,
+								}
 							}),
 					)
 
 					if (!commits_.length) return null
-					return { title: config.emoji ? title : title.slice(3), commits: commits_ }
+					return {
+						title: config.emoji ? title : title.slice(3),
+						commits: commits_,
+					}
 				}),
 		)
 	).filter(Boolean) as Section[]
