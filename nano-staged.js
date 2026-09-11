2f88376361d9b01563bb565a1277007a01346483
Isa <hi@okinea.dev>
2026-09-11 09:20:40 +0200

    🪪 ci(`release.yml`): fix authentication workflow

--- nano-staged.js
+++ nano-staged.js
@@ -1,7 +1,7 @@
 // oxlint-disable-next-line jsdoc/check-tag-names
 /** @type {import('nano-staged').Configuration} */
 const config = {
-	'*': ['bun run format --no-error-on-unmatched-pattern', 'cspell'],
+	'*': ['bun run format --no-error-on-unmatched-pattern', 'cspell --no-must-find-files'],
 	'*.{ts,vue}': () => ['bun run lint:oxc'],
 }
 
